e8fb9ce8fe404c7ae822da9625d87a26a1448e4e
Regina Kwee <Regina.Kwee@cern.ch>
2013-06-27 11:24:49 +0200

    adding submit and merge scripts

---
+++ AnalysisScripts/py/merge.py
@@ -0,0 +1,203 @@
+#!/usr/bin/python
+#
+
+# Regina Kwee-Hinzmann @ RHUL
+# 2013, June
+# -----------------------------------------------------------------
+import os, stat, sys
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("-r", "", dest="rundir", type="string",
+                  help="this is the main rundir with the run_X dirs")
+
+(options, args) = parser.parse_args()
+rundir = options.rundir
+
+# rundir = '/afs/cern.ch/work/r/rkwee/public/sixtrack_example/'
+
+# 2 types of merging: 1. append, 2. add up
+
+# all files to append:
+fApp = ['LPI_BLP_out.s',           
+        'FirstImpacts.dat',
+        'survival.dat',
+        'impacts_fake.dat',
+        'impacts_real.dat',
+        ]
+
+# specified to work only with this file!
+fAdd = ['coll_summary.dat',
+        ]
+
+debug = 0
+# -----------------------------------------------------------------
+def findGoodFiles(targetfile):
+
+    # all goodfiles
+    resFiles = []
+
+    # find the correct path 
+    subdirs = os.listdir(rundir)
+
+    # ------------
+    # get all good files into a list
+
+    for subdir in subdirs:
+
+        if debug:        
+            print("Finding " + subdir)
+
+        # directory with results 
+        rdir = rundir + subdir + "/"
+
+        if not os.path.isdir(rdir):
+            continue
+
+        thisfile = rdir + targetfile
+
+        if not os.path.exists(thisfile):
+            continue
+
+        resFiles += [thisfile]
+
+    return resFiles
+
+# -----------------------------------------------------------------
+def doAppend(fApp):
+
+    print("Am in " + rundir)
+
+    for targetfile in fApp:
+
+        foutname = rundir + targetfile.split('.')[0] + '_merged.' + targetfile.split('.')[-1]
+        print('-'*20)
+        print("Writing summary file " + foutname)
+
+        # open one new file for merged info
+        fileout  = open(foutname,'w')
+
+        resFiles = findGoodFiles(targetfile)
+        cnt      = 0
+
+        # ------------
+        for rFile in resFiles:
+
+            cnt += 1
+
+            if debug:
+                print("opening this file" + thisfile)
+            
+            with open(rFile) as rf:
+
+                for line in rf:
+
+                    if cnt < 2 and line.count("#"):
+                        fileout.write(line)
+
+                    if not line.count('#'):
+                        fileout.write(line)
+
+        fileout.close()
+
+# -----------------------------------------------------------------
+                    
+def doAddup(fAdd):
+
+    # ---------------
+    # this is for coll_summary.dat only!
+    # ---------------
+
+    for targetfile in fAdd:
+
+        foutname = rundir + targetfile.split('.')[0] + '_merged.' + targetfile.split('.')[-1]
+        print('-'*20)
+        print("Writing summary file " + foutname)
+
+        # open one new file for merged info
+        fileout  = open(foutname,'w')
+
+        resFiles = findGoodFiles(targetfile)
+        cnt      = 0
+        icoll, collname, length = '','',''
+        nimp, nabs, imp_av, imp_sig = -9999.,-9999.,-9999.,-9999.
+        
+        # ------------
+        for rFile in resFiles:
+
+            cnt += 1
+
+            # if debug:
+            print("opening file" + rFile)
+            
+            if cnt == 1:
+                # create dict while reading first file
+                allLines = []
+
+            with open(rFile) as rf:
+                
+                for line in rf:
+
+                    if cnt < 2 and line.count("#"):
+                        fileout.write(line)
+
+                    if not line.count('#'):
+                        
+                        icoll    = line.split()[0]
+                        collname = line.split()[1]
+                        nimp     = float(line.split()[2])
+                        nabs     = float(line.split()[3])
+                        imp_av   = float(line.split()[4])
+                        imp_sig  = float(line.split()[5])
+                        length   = line.split()[6]
+
+                        # single line     # 0        1     2      3      4        5
+                        sline = [ icoll, [collname, nimp, nabs, imp_av, imp_sig, length] ]
+
+                        if cnt == 1:
+                            allLines += [sline]
+                        else:
+                            if debug:
+                                print(cnt, icoll, nimp, cDict[icoll])
+
+                            # dict already exists, so average and add up
+                            if cDict[icoll][1]:
+                                cDict[ icoll ][3]  = imp_av * nimp/(cDict[icoll][1] + nimp) + cDict[icoll][1]/(cDict[icoll][1]+nimp) * cDict[icoll][3]
+                                cDict[ icoll ][4]  = imp_sig* nimp/(cDict[icoll][1] + nimp) + cDict[icoll][1]/(cDict[icoll][1]+nimp) * cDict[icoll][4]
+
+                            cDict[ icoll ][1] += nimp
+                            cDict[ icoll ][2] += nabs
+
+                            
+            # create once the dict
+            if cnt == 1:
+                cDict = dict(allLines)
+        
+        # ----------
+        
+        # use sorted keys
+        cKeys = [str(i) for i in range(73)]
+
+        for k in cKeys:
+
+            try:
+
+                #print cDict[k][0]
+                line ='{0:3s} {1:15s} {2:10} {3:10} {4:15f}E-3 {5:10f}E-3    {6:10s}'.format(k, cDict[k][0], cDict[k][1], cDict[k][2], cDict[k][3]*1e3, cDict[k][4]*1e3, cDict[k][5])
+
+                #print line
+                fileout.write(line + '\n')
+
+
+            except KeyError:
+                pass
+
+
+        fileout.close()
+
+        
+# -----------------------------------------------------------------
+if __name__ == "__main__":
+
+    doAppend(fApp)
+    doAddup(fAdd)
---
+++ AnalysisScripts/py/submitSixTrack_lxbatch.py
@@ -0,0 +1,136 @@
+#!/usr/bin/python
+#
+# this is a script to submit sixtrack jobs to lxbatch
+# it needs a source dir with all the input files required for the job
+# these are copied to the dir which is allocated randomly by the batch sytem
+#
+# a copy of the run structure is created on afs
+# result files are copied back to that afs structure
+#
+# this script recognizes the directories already created in the main run_dir
+# it will create new subdirs in run_dir counting on top of the exisisting ones
+#
+# Regina Kwee-Hinzmann
+# 2013, June
+#
+# -----------------------------------------------------------
+
+import os, stat, sys
+
+njobs=10
+queuename='8nh'
+npacks='50'
+doRun=1
+showInfo=0
+# -----------------------------------------------------------
+afsRunMain  = "/afs/cern.ch/work/r/rkwee/HL-LHC/runs/"
+run_dir     = "exampleRuns/"
+source_dir  = "/afs/cern.ch/work/r/rkwee/public/sixtrack_example/clean_input/save/"
+afs_run_dir = afsRunMain + run_dir
+# -----------------------------------------------------------
+
+if not os.path.exists(afs_run_dir):
+    print 'making dir', afs_run_dir
+    os.mkdir(afs_run_dir)
+
+# prepare runfiles: these files should be present in source_dir
+
+sixtrackExe = 'SixTrack_4411_coll_gfortran_O4'
+fort2       = 'fort.2'
+fort3       = 'fort.3'
+collDB      = 'CollDB_V6.503_lowb_st.b1.data'
+collPos     = 'CollPositions.b1.dat'
+apertfile   = 'allapert.b1'
+surveyfile  = 'SurveyWithCrossing_XP_lowb.dat'
+beamlossExe = 'BeamLossPattern_2005-04-30_gcc2.9'
+cleanIneExe = 'CleanInelastic'
+cleancoll   = 'correct_coll_summary.sh'
+
+inputFiles  = [sixtrackExe,beamlossExe,cleanIneExe,fort2,collDB,collPos,apertfile,surveyfile,cleancoll]
+
+# -----------------------------------------------------------
+
+occupiedDirs = []
+
+for subdir in os.listdir(afs_run_dir):
+    if showInfo:
+        print "cheking", subdir
+    if os.path.exists(afs_run_dir+subdir):
+        occupiedDirs += [int(subdir.split('_')[-1])]
+
+startjob = 0
+if occupiedDirs:
+    startjob = max(occupiedDirs)
+
+if showInfo:
+    print "occupiedDirs", occupiedDirs
+    print("starting in folder run_" + str(startjob))
+
+newrange = range(startjob+1, startjob+njobs+1)
+
+if showInfo:
+    print "newrange", newrange
+# -----------------------------------------------------------
+for job in newrange:
+
+    # for each job create a subdir
+    subdir = afs_run_dir + 'run_' + str(job) + '/'
+    
+    os.mkdir(subdir)
+
+    print('Changing to rundir ' + subdir)
+    os.chdir(subdir)
+    
+    # write a job script
+
+    run_job_fname = subdir + 'job_'+str(job)+'.sh'
+    run_job = open(run_job_fname,'w')
+    run_job.write('#!/bin/bash\n\n')
+
+    run_job.write('mkdir ' + run_dir +'\n')
+    run_job.write('mkdir ' + run_dir +'run_' + str(job)+ '\n')
+    run_job.write('cd ' + run_dir + 'run_'+ str(job) +'\n')
+
+    # copy the inputfiles
+    for inpfile in inputFiles:
+        
+        inpfile = source_dir + inpfile
+
+        # copy to the local, randomly attributed path on the lxbatch
+        cmd =  'cp ' + inpfile + ' . \n'
+        run_job.write(cmd)
+
+    # now fort3 file
+    cmd = "sed 's\\100 3500000\\" + npacks + " 3500000\\' " + source_dir+fort3 + " > " + fort3 + '\n'
+    run_job.write(cmd)
+
+    run_job.write('./' +sixtrackExe + ' >| screenout\n' ) 
+    run_job.write('./' +beamlossExe + ' lowb tracks2.dat BLP_out ' + apertfile  + '\n')
+    run_job.write('./' +cleanIneExe + ' FLUKA_impacts.dat LPI_BLP_out.s '+ collPos + ' coll_summary.dat\n')
+    run_job.write('./' + cleancoll + '\n')
+
+    # copy back
+    cmd = 'cp amplitude.dat efficiency.dat coll_summary.dat screen* survival.dat LPI* FLUKA* FirstImpacts.dat sigmasettings.out impacts* ' + subdir
+    #cmd = "cp * " + subdir + "\n"
+    run_job.write(cmd)
+
+    run_job.close()
+
+    # make it executable 
+    cmd = 'chmod 755 ' + run_job_fname
+    os.system(cmd)
+
+    # submit to batch
+    cmd = 'bsub -q ' + queuename + ' -R "rusage[pool=30000]" < ' + run_job_fname
+    print cmd
+
+    if doRun:        
+        os.system(cmd)
+    
+# -----------------------------------------------------------
+
+cnt = len(newrange)
+if doRun:
+    print "submitted", cnt, "jobs"
+else:
+    print "would have submitted", cnt, "jobs"
