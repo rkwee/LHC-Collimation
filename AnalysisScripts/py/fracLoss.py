b499e7cfe822e129fe1109ba9cfa8d7aeb49c7dd
Regina Kwee <Regina.Kwee@cern.ch>
2014-11-07 14:17:11 +0100

    fix in nprim

--- AnalysisScripts/py/fracLoss.py
+++ AnalysisScripts/py/fracLoss.py
@@ -2,33 +2,33 @@
 
 # hHalo
 lossesH = [2,31,1177,56]
-nprim  = 9934100*64.
+nprim  = 993400*64.
 
 #vHalo 
 lossesV = [0,48,1238,103]
-nprim  += 9942100.*64
+nprim  += 994200.*64
 
-# sum
+# # sum
 losses = [lossesH[i]+lossesV[i] for i in range(len(lossesH))]
 
-# old hHalo with maybe some bias
-losses = [2,28,769,46]
-nprim  = 687400.*64
+# # old hHalo with maybe some bias
+# losses = [2,28,769,46]
+# nprim  = 687400.*64
 
-# TCTOUT
+# # TCTOUT
 
-# vHalo
-lossesV = [6,721,1141,1162]
-nprim   = 14742*50.*64
+# # vHalo
+# lossesV = [6,721,1141,1162]
+# nprim   = 14742*50.*64
 
-lossesH = [21,1221,7085,676]
-nprim   += 17237.*50*64
+# lossesH = [21,1221,7085,676]
+# nprim   += 17237.*50*64
 
-# TCTin
+# # TCTin
 lossesH = [7517,1030,13,1443]
-nprim  = 9934100*64.
+nprim  = 993400*64.
 lossesV = [333,1719,8,969]
-nprim  += 9942100.*64
+nprim  += 994200.*64
 
 
 # sum
