51225c3a87fd7f970021161fc359d64cf66f48f1
Regina Kwee <Regina.Kwee@cern.ch>
2015-01-29 10:57:29 +0100

    add tracks2 format

--- AnalysisScripts/py/createTTree.py
+++ AnalysisScripts/py/createTTree.py
@@ -11,6 +11,7 @@ treeName     = "particle"
 fortformat66 = "event/I:generation/I:particle/I:energy_ke/F:weight/F:x/F:y/F:xp/F:yp/F:age/F:energy_tot/F:x_interact/F:y_interact/F:z_interact/F:t_interact/F"
 fortformat30 = "event/I:particle/I:generation/I:weight/F:x/F:y/F:xp/F:yp/F:energy_tot/F:energy_ke/F:age/F:x_interact/F:y_interact/F:z_interact/F"
 impactsformat= "icoll/I:c_rotation/F:s/F:x/F:xp/F:y/F:yp/F:nabs/I:np/I:ntu/I"
+tracks2format= "icoll/I:turn/I:s/F:x/F:xp/F:y/F:yp/F:dEoverE/F:type"
 # ---------------------------------------------------------------------------------
 # fname (=input) are the merged fort.66 or fort.30 file from fluka runs"
 def ctree(fname):
@@ -19,17 +20,21 @@ def ctree(fname):
 
     if fname.endswith("30"): 
         fortformat = fortformat30
-        if debug: print "Using HL format", '.'*10
+
     elif fname.endswith("66"): 
         fortformat = fortformat66
-        if debug: print "Using 4 TeV format", '.'*10
+
     elif fname.count('impacts_real'):
         fortformat = impactsformat
-        if debug: print "Using format for impacts real file", '.'*10
-        else:
-            print "no format defined."
-            return
 
+    elif fname.count("tracks2"):
+        fortformat = tracks2format
+
+    else:
+        print "no format defined."
+        return
+
+    print("Reading file", fname, "/n Using format", fortformat)
     # -----------------------------------------------------------------------------
     
     rfoutName = fname +".root"
@@ -40,7 +45,7 @@ def ctree(fname):
     mytree.ReadFile(fname,fortformat)
 
     #st*d root
-    for i in range(100): myfile.Delete(treeName + ";" + str(i))
+    #for i in range(100): myfile.Delete(treeName + ";" + str(i))
 
     mytree.Write()
     myfile.Close()
