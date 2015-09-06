d857e248f39574d6746fe2581c77b18aa0b0d50d
Regina Kwee <Regina.Kwee@cern.ch>
2015-09-06 23:31:23 +0200

    bbgAnalysis: rm on the fly definition

--- AnalysisScripts/py/bbgAnalysis.py
+++ AnalysisScripts/py/bbgAnalysis.py
@@ -33,8 +33,9 @@ if __name__ == "__main__":
     #TTreeFileName = 'data/4TeV/ir1_4TeV_settings_from_TWISS_b2_nprim7825000_66.root'        
     # TTreeFileName = 'data/4TeV/ir1_4TeV_settings_from_TWISS_20MeV_b2_nprim5356000_66.root'
     # TTreeFileName = 'data/4TeV/ir1_4TeV_settings_from_TWISS_20GeV_b2_nprim158890000_66.root'
-    #TTreeFileName = 'data/4TeV/ir1_4TeV_settings_from_TWISS_20MeV_b1_nprim7964000_66.root'
-    #TTreeFileName = workpath + 'runs/4TeV_Halo/ir1_BH_4TeV_settings_from_TWISS_20MeV_b1_nprim7964000_66.root'
+    TTreeFileName = 'data/4TeV/ir1_BH_4TeV_settings_from_TWISS_20MeV_b1_nprim7964000_66.root'
+
+    # TTreeFileName = workpath + 'runs/4TeV_Halo/ir1_BH_4TeV_settings_from_TWISS_20MeV_b1_nprim7964000_66.root'
     #TTreeFileName = workpath + 'runs/FL_TCT5IN_roundthin/hilumi_ir1_hybrid_b1_20MeV_exp_nprim1635000_30.root'
     # TTreeFileName = workpath + 'runs/FL_TCT5IN_roundthinB1_2nd/hilumi_ir1_hybrid_b1_exp_20MeV_nprim5319000_30.root'
     # TTreeFileName = workpath + 'runs/FL_TCT5LOUT_roundthinB1_2nd/hilumi_ir1_hybrid_b1_exp_20MeV_nprim5350000_30.root'    
@@ -48,7 +49,6 @@ if __name__ == "__main__":
     # TTreeFileName = 'runBG_corr/ir1_BG_4TeV_settings_from_TWISS_20MeV_b1_nprim256500_67.root'
     #TTreeFileName = "/afs/cern.ch/project/lhc_mib/bbgen/4TeV/beamgas/withoutBeamSize/beam-gas_4TeV-IR1_to_arc_20MeV_cutoff_nprim28788000_66.root"
     #TTreeFileName = "data/ir1_BG_4TeV_settings_from_TWISS_20MeV_b1_nprim2952500_67.root"
-    if datafile.endswith("root"): TTreeFileName = datafile
     # for comparisons plot change in plotSpectra
     # ---------------------
     # define tag in helpers!
