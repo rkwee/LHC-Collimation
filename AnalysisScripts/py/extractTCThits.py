664ad31179553b3fef09461af28ee60084ce3a18
Regina Kwee <Regina.Kwee@cern.ch>
2015-10-15 17:20:15 +0200

    extractTCThits.py: modify to work from collgaps and unmerged files

--- AnalysisScripts/py/extractTCThits.py
+++ AnalysisScripts/py/extractTCThits.py
@@ -2,7 +2,7 @@
 #
 # Mar 2015, rkwee
 ## -------------------------------------------------------------------------------
-import helpers, gzip, os, subprocess
+import helpers, gzip, os, subprocess, sys
 from helpers import *
 
 from optparse import OptionParser
@@ -11,13 +11,77 @@ parser = OptionParser()
 parser.add_option("-d", dest="dirs", type="string",
                   help="directories with impacts real file")
 
-(options, args) = parser.parse_args()
 
+parser.add_option("-c", dest="cfile", type="string",
+                  help="collgaps file")
+
+
+(options, args) = parser.parse_args()
+cfile = options.cfile #= projectpath + 'offmom/LHC_6.5TeV/plus_500Hz/B1/collgap.lowb.hor.b1.dat'
 dirs = options.dirs
 #dirs = 'H5_HL*TCT5IN*relaxColl*vHaloB1*sround*'
 #dirs = 'pencil*B*'
 # -----------------------------------------------------------------------------------
-def extractHits(f):
+
+
+def extractFromOneFileGz(f,colls, cDict):
+
+    if not f.endswith("gz"): 
+        print('Please gzip file first')
+        sys.exit()
+
+    allcolls = cDict.keys()
+
+    for coll in colls:
+        if coll not in allcolls:
+            print "Did not find ", coll, "in ", collsummary
+            continue
+        mf = gzip.open(f)
+        icoll = cDict[coll][0]
+        print coll, icoll
+        filename = f.split('.dat.gz')[0] + '_' + icoll + '_' + coll + '.txt'
+        print('writing', filename)
+
+        hfile = open(filename, 'w')
+        for line in mf: 
+            if line.split()[0].count(icoll): 
+                hfile.write(line)
+
+        cmd = 'wc -l ' + filename
+        print cmd
+        os.system(cmd)
+
+def extractFromFilelistDat(colls,cDict,cfile):
+        
+    allcolls = cDict.keys()
+    for coll in colls:
+
+        if coll not in allcolls:
+            print "Did not find ", coll, "in ", cfile
+            continue
+
+        icoll = cDict[coll][0]
+        
+        filename =  cfile.split("/coll")[0] + '/impacts_real_' + icoll + '_' + coll + '.txt'
+        print('writing', filename)
+
+        hfile = open(filename, 'w')
+
+        cmd = "grep "
+        if len(str(icoll)) == 1:   cmd += '"^   '+ str(icoll) + ' "'
+        elif len(str(icoll)) == 2: cmd += '"^  '+ str(icoll) + ' "'
+
+        cmd += ' ' + dirs + '/imp*dat >| ' + filename
+        print cmd
+        os.system(cmd)
+
+
+        cmd = 'wc -l ' + filename
+        print cmd
+        os.system(cmd)
+
+# ----------------------------------------------------------------------------
+def extractHits(files):
 
     # -- HL runs
     colls = [
@@ -31,7 +95,7 @@ def extractHits(f):
          'TCTVA.4R1.B2',
          ]
 
-    colls = [
+    collsB2 = [
          'TCTH.5R5.B2',
          'TCTVA.5R5.B2',
          'TCTH.4R5.B2', 
@@ -48,74 +112,35 @@ def extractHits(f):
 
     # -- pencil beam run
 
-    # colls = [
+    colls = [
 
-    #     'TCP.6L3.B1', 
-    #     'TCTH.4L1.B1', 
-    #     'TCTVA.4L1.B1',
-    #     'TCTH.4L5.B1',  
-    #     'TCTVA.4L5.B1', 
+        'TCP.6L3.B1', 
+        'TCTH.4L1.B1', 
+        'TCTVA.4L1.B1',
+        'TCTH.4L5.B1',  
+        'TCTVA.4L5.B1', 
 
-    #     'TCP.6R3.B2',
-    #     'TCTH.4R5.B2',    
-    #     'TCTVA.4R5.B2',   
-    #     'TCTH.4R1.B2',    
-    #     'TCTVA.4R1.B2',   
-    #     ]
-    tag = f.split('/')[0]
-    tagOnly = tag.split('/')[-1]
-    collsummary = tag + '/coll_summary_' + tagOnly + '.dat'
-    print 'using this collsummary file', collsummary
+        'TCP.6R3.B2',
+        'TCTH.4R5.B2',    
+        'TCTVA.4R5.B2',   
+        'TCTH.4R1.B2',    
+        'TCTVA.4R1.B2',   
+        ]
+    # tag = f.split('/')[0]
+    # tagOnly = tag.split('/')[-1]
+    # collsummary = tag + '/coll_summary_' + tagOnly + '.dat'
+    #print 'using this collsummary file', collsummary
 
-    
-    cDict = collDict(collsummary)
-
-    allcolls = cDict.keys()
-
-    if f.endswith('gz'):
-
-        for coll in colls:
-            if coll not in allcolls:
-                print "Did not find ", coll, "in ", collsummary
-                continue
-            mf = gzip.open(f)
-            icoll = cDict[coll][0]
-            print coll, icoll
-            filename = f.split('.dat.gz')[0] + '_' + icoll + '_' + coll + '.txt'
-            print('writing', filename)
-            
-            hfile = open(filename, 'w')
-            for line in mf: 
-                if line.split()[0].count(icoll): 
-                    hfile.write(line)
-
-            cmd = 'wc -l ' + filename
-            print cmd
-            os.system(cmd)
-
+    if cfile.count("collgap"): 
+        cDict = collgapsDict(cfile)
+    elif cfile.count("coll_summary"):
+        cDict = collDict(cfile)
     else:
-        
-        for icoll,coll in colls:
-            with open(f) as mf:
-                
-                filename = f.split('.dat')[0] + '_' + icoll + '_' + coll + '.txt'
-                print('writing', filename)
-
-                hfile = open(filename, 'w')
-
-                for line in mf: 
-                    if line.split()[0].count(icoll): 
-                        hfile.write(line)
+        print "cannot define cfile", cfile
+        sys.exit()
 
 
-                cmd = 'wc -l ' + filename
-                print cmd
-                os.system(cmd)
-
-
-
-        # print('Please gzip file first')
-
+    extractFromFilelistDat(colls,cDict,cfile)
 # ----------------------------------------------------------------------------
 def normaliseTCThits():
 
@@ -158,17 +183,18 @@ def normaliseTCThits():
 # ----------------------------------------------------------------------------
 if __name__ == "__main__":
 
-    cmd = 'ls -1 ' + dirs + '/imp*gz'
+    cmd = 'ls -1 ' + dirs + '/imp*dat'
     process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
     myStdOut = process.stdout.read()
     files = myStdOut.split()
 
-    print "Taking ",len(files)," files: ", files
-    #files = []
+    print "Taking ",len(files)," files: "#, files
+    sfiles = [i for i in files[:3]]
     
-    for f in files: 
-        print '.'*30, f, '.'*30
-        extractHits(f)
+    files = sfiles
+    # for f in files: 
+    #     print '.'*30, f, '.'*30
+    #     extractHits(f)
 
-    #extractHits(f)
+    extractHits(files)
     #normaliseTCThits()
