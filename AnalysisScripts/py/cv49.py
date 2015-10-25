7b0daff39b189a4d51cfe345da6c412406b21aea
Regina Kwee <Regina.Kwee@cern.ch>
2015-10-25 12:48:42 +0100

    cv49: re-use to check heavy ion fragments (-8)

--- AnalysisScripts/py/cv49.py
+++ AnalysisScripts/py/cv49.py
@@ -1,117 +1,33 @@
 #!/usr/bin/python
 #
-# June 2015, rkwee
+# Oct 2015, rkwee
 ## -------------------------------------------------------------------------------
-# selects randomly 10 out of 1000 values per s-location : use to preprare input for beamgas with size
+# re-use this to dump -8 particles 
 ## -------------------------------------------------------------------------------
-import random, helpers, gzip, math, time
-from helpers import workpath
+import ROOT, helpers
+from ROOT import *
+
 # -----------------------------------------------------------------------------------
-debug = 0
-pathtofiles = '/afs/cern.ch/project/lhc_mib/beamgas/4TeV_beamsize/createTrajectories/'
-def createSelectionList(nSPos,nTraj,von,bis):
-
-    # select between von to bis
-    selList = []
-    for s in range(nSPos):
-
-        tList = []
-        while len(tList) != nTraj:
-            rndm = random.randint(von,bis)
-            if rndm not in tList:
-                tList += [rndm]
-
-        # sorting to avoid retrieving data from a lower block and then going back to retrieve data from an upper block 
-        tList.sort()
-
-        selList += [ tList ]
-        if debug: print "added ", selList[-1], 'to list'
-
-    return selList
 
 def cv49():
-    tbegin = time.time()
-    # -- out file name as input for fluka
-    foutname = 'downselected_fort.89.cv49'
 
-    print 'writing ... ', foutname
-    fout = open(foutname, 'w')
+    bbgFile = helpers.projectpath + "BG/FL_4TeV_BG_20GeV_10k/ir1_BG_bs_4TeV_settings_from_TWISS_20GeV_b1_nprim167850000_67.root"
 
-                  
-    # format: #0 CXTRCK, #1 CYTRCK, #2 CZTRCK, #3 XTRACK(0), #4 YTRACK(0), #5 ZTRACK(0), #6JTRACK, #7 NCASE, #8 ATRACK
+    bbgFile = helpers.projectpath + "bbgen/4TeV/beam-gas_4TeV-IR1_to_arc_20MeV_cutoff_nprim28788000.root"
 
-    #files = [pathtofiles + '100evts/run_0000' + str(i) + '/ir1_4TeV_settings_from_TWISS_20MeV_b1_orbitDumpICON001_fort.89.gz' for i in range(1)]
+    bbgFile = helpers.projectpath + "bbgen/4TeV/beamgas/ir1_BG_bs_4TeV_settings_from_TWISS_20MeV_b1_nprim5925000_67.root"
 
-    files = [pathtofiles + '100evts/run_0000' + str(i) + '/ir1_4TeV_settings_from_TWISS_20MeV_b1_orbitDumpICON001_fort.89.copy' for i in range(1)]
+    print "reading.....", bbgFile
 
-    #files = [ pathtofiles + '13evts/test.txt.gz' ]
+    rf = TFile.Open(bbgFile)
+    mt = rf.Get("particle")
 
-    # Nsel can be at max the total available number of trajectories
-    nsteps, Nsel, ntrajMax = 547589,2,13
-    #nsteps, Nsel, ntrajMax = 10,2,8
-    
-    stepsize = 0.0998809
-    selList = createSelectionList(nsteps,Nsel,1,ntrajMax)
+    hname = "minusAcht"
+    nbins, xmin, xmax = 10, -9.5, 0.5
+    hist  = TH1F(hname, hname, nbins, xmin, xmax )
 
-    if debug: print "using this selection list of length", len(selList), selList 
-
-    i = 0
-    t0 = time.time()
-    t1,t2,t3,t4,t5 = 0,0,0,0,0
-    for mfile in files:
-
-        kk = mfile.split('/ir1_4TeV_settings_from_TWISS_20MeV_b1_orbitD')[0]
-        fnb = kk[-1]
-        print 'opening....', fnb, mfile
-
-        t1 = time.time()
-     
-        # improve performance
-        #for line in mf.read().splitlines(True):
-        with open(mfile) as mf:
-
-            for line in mf:
-                i += 1
-
-                t2 = time.time()
-                # loop over entire list
-
-                for s,sList in enumerate(selList):
-                    continue
-                    for traj in sList:
-
-                        # line of interest
-                        lineOfInt = (traj-1) * nsteps + s+1
-                        if debug: print "line of interest is", lineOfInt, "which corresponds to traj", traj, "in block", s+1
-
-                        if i < lineOfInt: 
-
-                            if debug: print "want line", lineOfInt, '. Am at line', i
-                            break
-
-                        elif i > lineOfInt: continue
-
-                        line = line.rstrip()
-                        fout.write(line + ' ' + fnb + ' \n')
-
-                        if debug:
-                            ncase  = int(line.split()[7])
-                            ztrack = float(line.split()[5])
-                            strack = s*stepsize
-                            print "selected traj", ncase, 'at position', ztrack, "(original), ", strack, '(calculated). diff ', math.fabs(ztrack-strack)
-
-                t3 = time.time()
-        t4 = time.time()
-
-    fout.close()
-    t5 = time.time()
-
-    print(str(tbegin)+' time when scripts begins')
-    print(str(t0-tbegin)+' time until just before first loop.')
-    print(str(t1-tbegin)+' time until 1 gzip open call.')
-    print(str(t2-tbegin)+' time until looping over lines.')
-    print(str(t3-tbegin)+' time after jumping over empty list loop.')
-    print(str(t4-tbegin)+' time until out of line loop.')
-    print(str(t5-tbegin)+' time until out files loop (1 file only).')
-
+    var = "particle"
+    cut = " particle == -8"
+    mt.Project(hname, var, cut)
+    print hname, "has", hist.GetEntries(), " entries."
 
