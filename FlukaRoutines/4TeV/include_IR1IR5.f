9e10f5d7b52c7c862476c93f2e518b0001f056e1
Regina Kwee <Regina.Kwee@cern.ch>
2015-11-20 10:59:13 +0100

    clean up

--- FlukaRoutines/4TeV/include_IR1IR5.f
+++
@@ -1,3 +0,0 @@
-      COMMON / IR1IR5 /  xStart,yStart,zStart,tStart,runNr,tIn(3500000)
-
-      SAVE / IR1IR5 /
--- FlukaRoutines/4TeV/linkexec.sh
+++
@@ -1,6 +0,0 @@
-
-#/afs/cern.ch/work/r/rkwee/Fluka/fluka20112blinuxAA/flutil/lfluka -m fluka -o flukahp-production usrglo.f bmagfld.f mgdraw-interface-plane.f  source-forced-interactions-TCT.f
-/afs/cern.ch/work/r/rkwee/Fluka/fluka20112blinuxAA/flutil/lfluka -m fluka -o flukahp-production usrglo.f bmagfld.f mgdraw-interface-plane.f  source-forced-interactions-TCT.f
-#/afs/cern.ch/work/r/rkwee/Fluka/fluka20112blinuxAA/flutil/ldpm3qmd -m fluka -o flukahp-production usrglo.f bmagfld.f mgdraw-interface-plane.f  source-forced-interactions-TCT.f
-
-
