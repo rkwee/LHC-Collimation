f516becf4349afd9d0f4c9361b4a1a64c00aed29
Regina Kwee <Regina.Kwee@cern.ch>
2015-11-30 15:06:54 +0100

    cv59: before noise substraction

--- AnalysisScripts/py/cv59.py
+++ AnalysisScripts/py/cv59.py
@@ -4,8 +4,9 @@
 ## -------------------------------------------------------------------------------
 # plot data lossmap
 ## -------------------------------------------------------------------------------
-import helpers, subprocess
+import helpers, subprocess, ROOT
 from helpers import *
+from ROOT import *
 # -----------------------------------------------------------------------------------
 
 def cv59():
@@ -17,6 +18,12 @@ def cv59():
     thispath         +=  "BLMDataAnalysis/lossmaps/"
     rawDataFileName1 = thispath + "20120402__LOSS_RS09__180500_1333382700__Data_Extract_BLMs_all.txt"
     rawDataFileName2 = thispath + "20120402__LOSS_RS09__181500_1333383300__Data_Extract_BLMs_all.txt"
+
+    rawDataFileName3 = thispath + "20120701__LOSS_RS09__053500_1341113700__Data_Extract_BLMs_all.txt"
+    rawDataFileName4 = thispath + "20120701__LOSS_RS09__054500_1341114300__Data_Extract_BLMs_all.txt"
+    rawDataFileName5 = thispath + "20120701__LOSS_RS09__174500_1341157500__Data_Extract_BLMs_all.txt"
+    rawDataFileName6 = thispath + "20120701__LOSS_RS09__175503_1341158103__Data_Extract_BLMs_all.txt"
+
     blmPositions     = thispath + "BLMs_all_with_s_coordinate.txt"
     
     blmPositionsDict = createDictFromRow(blmPositions)
@@ -43,10 +50,21 @@ def cv59():
     # -- get a specific line
     format = "%Y-%m-%d %H:%M:%S"
     lossmaps_dt = [
-        ['B1H_afterSqueeze', '2012-04-02 18:17:38', rawDataFileName2],
-        ['B1V_afterSqueeze', '2012-04-02 18:16:07', rawDataFileName2],
-        # ['B2H_afterSqueeze', '2012-04-02 18:14:50', rawDataFileName1],
-        # ['B2V_afterSqueeze', '2012-04-02 18:13:29', rawDataFileName1],
+        # ['B1H_afterSqueeze_April', '2012-04-02 18:17:38', rawDataFileName2],
+        # ['B1V_afterSqueeze_April', '2012-04-02 18:16:07', rawDataFileName2],
+        # # ['B2H_afterSqueeze_April', '2012-04-02 18:14:50', rawDataFileName1],
+        # # ['B2V_afterSqueeze_April', '2012-04-02 18:13:29', rawDataFileName1],
+
+        # ['B1H_afterTS2', '2012-07-01 05:41:31', rawDataFileName3],
+        # ['B1V_afterTS2', '2012-07-01 05:37:08', rawDataFileName3],
+        # ['B2H_afterTS2', '2012-07-01 05:49:50', rawDataFileName4],
+        # ['B2V_afterTS2', '2012-07-01 05:47:59', rawDataFileName4],
+
+        ['B1H_afterSqueeze_July', '2012-07-01 18:00:26', rawDataFileName6],
+        ['B1V_afterSqueeze_July', '2012-07-01 18:04:43', rawDataFileName6],
+        ['B2H_afterSqueeze_July', '2012-07-01 17:52:46', rawDataFileName5],
+        ['B2V_afterSqueeze_July', '2012-07-01 17:57:17', rawDataFileName6],
+
         ]
 
     # has as key B1H_xxx and as content a dict [blmName]: measurement
@@ -85,14 +103,13 @@ def cv59():
         for i,blm in enumerate(blmsData):
             dataPerLossmap += [[blm, dataline[i] ]]
 
-        dataList += [[lossmapType, [ dict(dataPerLossmap), missingData, missingPos] ]]
         maxloss = max(dataline)
         indexmax = dataline.index(maxloss)
         blmMaxloss = blmsData[indexmax]
         print "Maximum loss", maxloss, "in ", blmMaxloss
 
         if maxloss > 1: 
-            # remove bad data, this changes dataline!
+            # remove bad data, this changes dataline
             dataline.pop(indexmax)
             # apply same change to the names
             blmsData.pop(indexmax)
@@ -102,21 +119,59 @@ def cv59():
             blmMaxloss = blmsData[indexmax]
             print "Unreasonable data loss in that blm. Next maximum is", maxloss, "in ", blmMaxloss
 
+        dataList += [[lossmapType, [ dict(dataPerLossmap), missingData, missingPos, maxloss, dt ] ]]
 
     # make it a dict
     dataDict = dict(dataList)
 
     print "."*99
+    cv = []
+    subfolder = 'TCT/4TeV/datalossmaps/'
     for dKey in dataDict.keys():
         print "-"*44, dKey,'-'*44
-        #print "length of ", dKey, "is", len(dataDict[dKey][0])
+        # print "length of ", dKey, "is", len(dataDict[dKey][0])
 
-        missingPos = dataDict[dKey][2]
+        # data dictionary
         dataPerLossmapDict = dataDict[dKey][0]
-        for i,blm in enumerate(missingPos):
-            print "Missing position of blm #",i, blm, ". Data is", dataPerLossmapDict[blm]
+        maxloss = dataDict[dKey][3]
+        dt = dataDict[dKey][4]
 
+        # check missing positions
+        missingPos = dataDict[dKey][2]
+        if missingPos:
+            for i,blm in enumerate(missingPos):
+                print "Missing position of blm #",i, blm, ". Data is", dataPerLossmapDict[blm]
 
+        # plot now good data
+        tag, nbins, xmin, xmax = dKey, 266590, 0.1, 26659.
+        blm_loss = TH1F("blm_loss" + tag,"blm_loss" + tag,nbins, xmin, xmax)
 
+        cv += [ TCanvas( 'cv'+tag, 'cv'+tag, 1500, 700) ]
+        cv[-1].SetLogy(1)
+        cv[-1].SetLeftMargin(0.2)
+        cv[-1].SetTopMargin(0.15)        
+        cv[-1].SetTopMargin(0.15)        
+        gStyle.SetOptStat(0)
 
-            
+        for blm in blmsPos:
+            spos = float(blmPositionsDict[blm][0])
+            val  = dataPerLossmapDict[blm]
+
+            #print "spos ", spos, val
+            blm_loss.Fill(spos,val)
+
+            # pname  = wwwpath
+            # pname += subfolder + tag +'.png'
+
+        blm_loss.Scale(1./maxloss)
+        blm_loss.GetYaxis().SetRangeUser(1e-8,1.1)
+        blm_loss.GetYaxis().SetTitle("local cleaning inefficiency")
+
+        blm_loss.Draw()
+        lab = mylabel(42)
+        lab.DrawLatex(0.3, 0.92, 'betatron lossmap at 4 TeV ' + dt)
+
+        pname  = thispath + tag +'.png'
+
+        print('Saving file as ' + pname ) 
+        cv[-1].SaveAs(pname)
