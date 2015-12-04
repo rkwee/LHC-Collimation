492aa71cb0b94df60b751f3e0feb7bbf9c240427
Regina Kwee <Regina.Kwee@cern.ch>
2015-12-04 14:28:04 +0100

    cv43: separate out last plot

--- AnalysisScripts/py/cv43.py
+++ AnalysisScripts/py/cv43.py
@@ -4,7 +4,7 @@
 ## -------------------------------------------------------------------------------
 # depths plots of FLUKA input for halo
 ## -------------------------------------------------------------------------------
-import ROOT, sys, os, time, math
+import ROOT, sys, os, time, math, subprocess
 from ROOT import *
 import helpers, array
 from helpers import *
@@ -21,17 +21,17 @@ def cv43():
     # flat beam HL
         [gitpath  + "SixTrackConfig/7TeV/hilumiLHC/TCThaloStudies_relaxedCollSettings/b1/collgaps.dat.TCT5LIN",\
              workpath + "runs/HL_TCT5INOUT_relSett/impacts_real_HL_TCT5IN_relaxColl_HaloB1_flatthin.txt.root", \
-             gitpath  + "SixTrackConfig/7TeV/hilumiLHC/TCThaloStudies_relaxedCollSettings/b1/collgaps.dat.TCT5LIN",\
+             workpath + "runs/HL_TCT5INOUT_relSett/H5_HL_TCT5IN_relaxColl_hHaloB1_flatthin/run_test/collgaps.dat",\
              'TCT/HL/relaxedColl/newScatt/', 'HL flat B1 ', 1., \
-             100,0., 20.,
+             100,0., 10.,
             ],
 
         # nomCollSett HL
         [ projectpath + 'HL1.0/H5_HL_nomSett_hHalo_b1/coll_summary_H5_HL_nomSett_hHalo_b1.dat',\
               projectpath + 'HL1.0/impacts_real_HL_TCT5IN_nomColl_haloB1.txt.root',\
               projectpath + 'HL1.0/test/run_00002/collgaps.dat',\
-              'TCT/HL/nominalColl/2015/', 'HL TCT5IN Halo B1', 1.,
-              100,0., 5.,
+              'TCT/HL/nominalColl/2015/', 'HL TCT5IN B1', 1.,
+              100,0., 10.,
             ],
 
         [ workpath + "runs/H5_HL_TCT5IN_relaxColl_hHaloB1_roundthin/coll_summary_H5_HL_TCT5IN_relaxColl_hHaloB1_roundthin.dat", \
@@ -39,7 +39,7 @@ def cv43():
               gitpath  + "SixTrackConfig/7TeV/hilumiLHC/TCThaloStudies_relaxedCollSettings/b1/collgaps.dat.TCT5LIN",\
               'TCT/HL/relaxedColl/newScatt/', \
               'HL 7 TeV', 0.1,\
-              100,0., 5.,
+              100,0., 10.,
           ],
 
        [ workpath + "runs/H5_HL_TCT5IN_relaxColl_hHaloB1_roundthin/coll_summary_H5_HL_TCT5IN_relaxColl_hHaloB1_roundthin.dat", \
@@ -47,7 +47,7 @@ def cv43():
               gitpath  + "SixTrackConfig/7TeV/hilumiLHC/TCThaloStudies_relaxedCollSettings/b1/collgaps.dat.TCT5LIN",\
               'TCT/HL/relaxedColl/newScatt/', \
               'HL 7 TeV', 0.1, \
-              100,0., 5.,
+              100,0., 10.,
           ],
 
         [ workpath + "runs/H5_HL_TCT5LOUT_relaxColl_hHaloB1_roundthin/coll_summary_H5_HL_TCT5LOUT_relaxColl_hHaloB1_roundthin.dat", \
@@ -55,7 +55,7 @@ def cv43():
               gitpath  + "SixTrackConfig/7TeV/hilumiLHC/TCThaloStudies_relaxedCollSettings/b1/collgaps.dat.TCT5LOFF",\
               'TCT/HL/relaxedColl/newScatt/', \
               'HL 7 TeV', 0.07, \
-              100,0., 20.,
+              100,0., 10.,
           ],
 
         [ gitpath + 'SixTrackConfig/7TeV/hilumiLHC/TCThaloStudies_relaxedCollSettings/b2/collgaps.dat.TCT5LIN',\
@@ -63,7 +63,7 @@ def cv43():
               gitpath + 'SixTrackConfig/7TeV/hilumiLHC/TCThaloStudies_relaxedCollSettings/b2/collgaps.dat.TCT5LIN',\
               'TCT/HL/relaxedColl/newScatt/', \
               'HL 7 TeV', 0.07, \
-              100,0., 5.,
+              100,0., 10.,
           ],
 
         [ gitpath + 'SixTrackConfig/7TeV/hilumiLHC/TCThaloStudies_relaxedCollSettings/b2/collgaps.dat.TCT5LOUT',\
@@ -129,16 +129,16 @@ def cv43():
         [ projectpath + 'tct_simulations/jobs/coll_summary.dat',\
               projectpath + 'tct_simulations/jobs/impacts_real_tct5inb1_crabs.dat.root', \
               projectpath + 'tct_simulations/jobs/collgaps.dat',\
-              'TCT/HL/crabcf/v3/', 'HL crabs failure - TCT5 in', 1., \
-              100,0., 5.,
+              'TCT/HL/crabcf/v3/', 'HL crabs failure', 1., \
+              100,0., 10.,
             ],
 
         # crabs tct5 out
         [ projectpath + 'tct_simulations/no_tct5_jobs/Kyrre/coll_summary.dat',\
               projectpath + 'tct_simulations/no_tct5_jobs/Kyrre/impacts_real_tct5otb1_crabs.dat.root', \
               projectpath + 'tct_simulations/no_tct5_jobs/Kyrre/collgaps.dat',\
-              'TCT/HL/crabcf/v3/tct5otrd/', 'HL crabs failure - TCT4 only', 1.,
-              100,0., 5.,
+              'TCT/HL/crabcf/v3/tct5otrd/', 'HL crabs failure', 1.,
+              100,0., 10.,
             ],
 
         # Hectors off momentum 4 TeV, minus 500Hz, IR1, B1
@@ -245,13 +245,12 @@ def cv43():
 
         showInfo = 0
         # canvas
-        a,b = 1,len(hx) + 1
+        a,b = 1,len(hx)
         cv = TCanvas( 'cv', 'cv', a*900, b*700)
         cv.Divide(a,b)
         cv.SetRightMargin(0.3)
         cv.SetLeftMargin(0.2)
         cv.SetTopMargin(0.15)
-        # only entries!
         gStyle.SetOptStat(0)
 
         x1, y1, x2, y2 = 0.2, 0.98, 0.84, 0.9
@@ -274,7 +273,16 @@ def cv43():
 
             lab.DrawLatex(x1, y1-0.1, hname)
             lab.DrawLatex(x1+0.45, y1-0.1, "entries " + str(int(entries)))
-            lab.DrawLatex(0.14, y2+0.1, rel)
+            #lab.DrawLatex(0.14, y2+0.1, rel)
+            lab.DrawLatex(x1, y2+0.08, energy)
+
+        pname  = wwwpath
+        pname += subfolder + 'inelposition_'+rel+'.png'
+        print('Saving file as ' + pname ) 
+        cv.SaveAs(pname)
+
+        # do a separate canvas for summary plot 
+        cvSum = TCanvas( 'cvSum', 'cvSum', 900, 700)
 
         x1, y1, x2, y2 = 0.48,0.7,0.9,0.93
         thelegend = TLegend( x1, y1, x2, y2)
@@ -290,9 +298,9 @@ def cv43():
 
         for i,hist in enumerate(hy):
 
-            cv.cd(len(hx)+1)
-            pad = cv.GetPad(len(hx)+1)
-            pad.SetLogy(1)
+            cvSum.cd(1)
+            cvSum.SetLogy(1)
+
             if not i: dOpt = 'hist'
             else: dOpt = 'histsame'
 
@@ -312,11 +320,11 @@ def cv43():
 
         thelegend.Draw()
         lab.DrawLatex(0.2, y2-0.05, energy)
-        pname  = wwwpath
-        pname += subfolder + 'inelposition_'+rel+'.png'
 
-        print('Saving file as ' + pname ) 
-        cv.SaveAs(pname)
+        pname  = wwwpath
+        pname += subfolder + 'inelposition_sum_'+rel+'.png'
+        pname = '/afs/cern.ch/work/r/rkwee/HL-LHC/LHC-Collimation/Documentation/ATS/HLHaloBackgroundNote/figures/inelposition_sum_'+rel+'.pdf'
+        cvSum.SaveAs(pname)
     # -----------------------------------------------------------------------------------
     # get the histograms
     for myset in sets:
@@ -345,6 +353,7 @@ def cv43():
 
         for collName in colls:
 
+            print '.' * 99
             try:
                 collid = cDict[collName][0]
             except KeyError:
@@ -352,6 +361,14 @@ def cv43():
                 continue
 
             cut = 'icoll == ' + collid
+
+            # print "Found collid", collid, "for ", collName
+            # cmd = 'grep "  ' + str(collid) + '" ' + cfile
+            # process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+            # tcts = process.stdout.read()
+            # tcts = tcts.split()
+            # print 'verify collid', cmd, "gives ", tcts
+
 
             # get halfgap in mm as x and y are in m.
             halfgap = float(cgDict[collName][5]) * 1000.
