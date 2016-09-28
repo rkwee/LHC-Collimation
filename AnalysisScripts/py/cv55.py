ec397b59b8015fedaa1c931146cca22d9517c94e
Regina Kwee <Regina.Kwee@cern.ch>
2016-09-28 23:38:14 +0200

    cv55: minor changes - wrong kind of check

--- AnalysisScripts/py/cv55.py
+++ AnalysisScripts/py/cv55.py
@@ -15,12 +15,12 @@ def cv55():
     showInfo = 1
 
     # format is: XIN(NIN), YIN(NIN), ZIN(NIN), UIN(NIN), VIN(NIN),TIN(NIN)
-    bgInput = '/afs/cern.ch/project/lhc_mib/beamsize/4TeV_beamsize/BGAS10.dat.root'
-    #bgInput = ' /afs/cern.ch/work/r/rkwee/HL-LHC/LHC-Collimation/FlukaRoutines/6.5TeV/beamgas/BGASB.dat.root'
+    #    bgInput = '/afs/cern.ch/project/lhc_mib/beamsize/4TeV_beamsize/BGAS10.dat.root'
+    bgInput = ' /afs/cern.ch/work/r/rkwee/HL-LHC/LHC-Collimation/FlukaRoutines/6.5TeV/beamgas/BGASB.dat.root'
     #bgInput = ' /afs/cern.ch/project/lhc_mib/beamgas/6500GeV_beamsize/downselected_fort.89.10.cv53.root'
-    #bgInput = ' /afs/cern.ch/project/lhc_mib/merge/awked_fort.89.29m.txt.root'
-    bgInput = ' /afs/cern.ch/project/lhc_mib/merge/awked_fort.89.44050.txt.root' # gives seg fault
-    bgInput = ' /afs/cern.ch/project/lhc_mib/merge/awked_fort.89_44.txt.root'
+    #    bgInput = ' /afs/cern.ch/project/lhc_mib/merge/awked_fort.89.29m.txt.root'
+    #bgInput = ' /afs/cern.ch/project/lhc_mib/merge/awked_fort.89.44050.txt.root' # gives seg fault
+    #bgInput = ' /afs/cern.ch/project/lhc_mib/merge/awked_fort.89_44.txt.root'
     rf = TFile.Open(bgInput)
     mt = rf.Get(treeName)
 
@@ -36,15 +36,15 @@ def cv55():
         # 'xp_MQXA.1R1':[dircosX, 100,-3.e-4,3.e-4, zVar + " > 29.3345e2 && "+ zVar +" < 29.3355e2",'xp [rad]', 'entries', "s = 29.335 m,xp = -9.1e-10 rad,#sigma_{xp} = 5.9e-07 rad"],
         # 'yp_MQXA.1R1':[dircosY, 100,0.1e-3,1.e-4, zVar + " > 29.3345e2 && "+ zVar +" < 29.3355e2",'yp [rad]', 'entries', "s = 29.335 m,yp = -5.4374e-05 rad,#sigma_{yp} = 8.0e-07 rad"],
 
- #       'x_MQXB.B2R1':[xVar, 100,-1.,1., zVar + " > 44.04999e2 && "+ zVar +" < 44.05001e2",'x [cm]', 'entries', "s = 44.05 m,#sigma_{x} = 0.11212 cm"],
-  #      'y_MQXB.B2R1':[yVar, 100,-.5,1.5, zVar + " > 44.04999e2 && "+ zVar +" < 44.05001e2",'y [cm]', 'entries', "s = 44.05 m,#sigma_{y} = 0.0850 cm"],
-        'xp_MQXB.B2R1':[dircosX, 100,-3.e-4,3.e-4, zVar + " > 44.04999e2 && "+ zVar +" < 44.05001e2",'xp [rad]', 'entries', "s = 44.05 m,xp = 6.99e-10 rad,#sigma_{xp} = 4.5e-07 rad"],
-#        'yp_MQXB.B2R1':[dircosY, 100,0.1e-3,1.e-4, zVar + " > 44.04999e2 && "+ zVar +" < 44.05001e2",'yp [rad]', 'entries', "s = 44.05 m,yp = 0.292e-3 rad,#sigma_{yp} = 5.9e-07 rad"],
+#         'x_MQXB.B2R1':[xVar, 100,-1.,1., zVar + " > 44.04999e2 && "+ zVar +" < 44.05001e2",'x [cm]', 'entries', "s = 44.05 m,#sigma_{x} = 0.11212 cm"],
+# #        'y_MQXB.B2R1':[yVar, 100,-.5,1.5, zVar + " > 44.04999e2 && "+ zVar +" < 44.05001e2",'y [cm]', 'entries', "s = 44.05 m,#sigma_{y} = 0.0850 cm"],
+#         # 'xp_MQXB.B2R1':[dircosX, 100,-3.e-4,3.e-4, zVar + " > 44.04999e2 && "+ zVar +" < 44.05001e2",'xp [rad]', 'entries', "s = 44.05 m,xp = 6.99e-10 rad,#sigma_{xp} = 4.5e-07 rad"],
+# #        'yp_MQXB.B2R1':[dircosY, 100,0.1e-3,1.e-4, zVar + " > 44.04999e2 && "+ zVar +" < 44.05001e2",'yp [rad]', 'entries', "s = 44.05 m,yp = 0.292e-3 rad,#sigma_{yp} = 5.9e-07 rad"],
 
-        # 'x_MQXA.3R1':[xVar, 100,-1.,1., zVar + " > 53.e2  && "+ zVar +" < 53.8e2",'x [cm]', 'entries', "s = 53.335 m,#sigma_{x} = 0.08211 cm"],
-        # 'y_MQXA.3R1':[yVar, 100,-.5,1.5, zVar + " > 53.e2  && "+ zVar +" < 53.8e2",'y [cm]', 'entries', "s = 53.335 m,#sigma_{y} = 0.1165 cm"],
-        # 'xp_MQXA.3R1':[dircosX, 100,-1e-5,1.e-5, zVar + " > 53.e2  && "+ zVar +" < 53.8e2",'xp [rad]', 'entries', "s = 53.335 m,xp = 5e-12 rad"],
-        # 'yp_MQXA.3R1':[dircosY, 100,-10e-5,-1.e-4, zVar + " > 53.e2  && "+ zVar +" < 53.8e2",'yp [rad]', 'entries', "s = 53.335 m,yp = -4.969e-05 rad"],
+        # 'x_MQXA.3R1':[xVar, 20,-0.6, 0.6, zVar + " > 53.3e2  && "+ zVar +" < 53.4e2",'x [cm]', 'entries', "s = 53.335 m,#sigma_{x} = 0.08211 cm"],
+        # 'y_MQXA.3R1':[yVar, 20,0.,1.4, zVar + " > 53.3e2  && "+ zVar +" < 53.4e2",'y [cm]', 'entries', "s = 53.335 m,#sigma_{y} = 0.1165 cm"],
+#        'xp_MQXA.3R1':[dircosX, 100,-1e-5,1.e-5, zVar + " > 53.3e2  && "+ zVar +" < 53.4e2",'xp [rad]', 'entries', "s = 53.335 m,xp = 5e-12 rad"],
+        'yp_MQXA.3R1':[dircosY, 20,1e-6,-1.e-4 , zVar + " > 53.3e2  && "+ zVar +" < 53.4e2",'yp [rad]', 'entries', "s = 53.335 m,yp = 4.969e-05 rad"],
 
         }
 
@@ -52,8 +52,8 @@ def cv55():
     x1, y1, x2, y2 = 0.2, 0.98, 0.84, 0.9
     for hname in hDict.keys():
 
-        xnbins, xmin, xmax = hDict[hname][1],hDict[hname][2],hDict[hname][3]
-
+        xnbins, xmin, xmax     = hDict[hname][1],hDict[hname][2],hDict[hname][3]
+        print   xnbins, xmin, xmax
         hist = TH1F(hname, hname, xnbins, xmin, xmax)
 
         xtitle, ytitle = hDict[hname][5],hDict[hname][6]
@@ -105,9 +105,12 @@ def cv55():
             if len(hDict[hname][7].split(",")) > 2:
                 lab.DrawLatex(x1, y1-0.25, hDict[hname][7].split(",")[2])
 
-        pname  = wwwpath + "TCT/6.5TeV/beamgas/"
-        #pname  = wwwpath + "TCT/4TeV/beamgas/"
-        pname += hname + ".png"
+        if bgInput.count("6.5TeV"):
+            pname  = wwwpath + "TCT/6.5TeV/beamgas/"
+        else:
+            pname  = wwwpath + "TCT/4TeV/beamgas/"
+        pname += hname.replace('.', '_') + ".pdf"
+
         cv.SaveAs(pname)
 
 
