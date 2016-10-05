7f6bee835274ad421a03dc11be73df51cb878c97
Regina Kwee <Regina.Kwee@cern.ch>
2016-10-05 13:04:32 +0200

    cv80+81 for 6.5 TeV beamgas re-weightening like cv70+71

---
+++ AnalysisScripts/py/cv80.py
@@ -0,0 +1,171 @@
+#!/usr/bin/python
+#
+# reweights by pressure profile 6.5 TeV
+# write rootfile with 2d histos flat and reweighted
+# from 4 TeV version cv70
+
+# Oct 16
+#
+# R Kwee, 2016
+# 
+# ---------------------------------------------------------------------------------
+import ROOT, sys, glob, os, math, helpers
+from ROOT import *
+from array import array
+from fillTTree import *
+from fillTTree_dict import generate_sDict
+from helpers import makeTGraph, mylabel, wwwpath
+import cv79
+from cv79 import pData
+# --------------------------------------------------------------------------------
+def resultFileBG(k,rel):
+    n = os.path.join(os.path.dirname(k),"results_pressure2015_"+rel+k.split('/')[-1])
+    return  n
+# --------------------------------------------------------------------------------
+def cv80():
+    print pData
+    pFile, xtitle,lText = pData[0]
+    datafile = '/Users/rkwee/Documents/RHUL/work/HL-LHC/runs/TCT/ir1_BG_bs_6500GeV_b1_20MeV_nprim3198000_67'
+    bbgFile = datafile + ".root"
+    print "Opening", bbgFile
+    tag = '_BG_6500GeV_flat_20MeV_bs' #!! MMMeV
+    norm = float(bbgFile.split('nprim')[-1].split('_')[0])
+    rfile = TFile.Open(bbgFile, "READ")
+    tBBG = rfile.Get("particle")
+    yrel = ''
+    print tBBG
+    sDict = generate_sDict(tag, norm, tBBG, yrel)
+
+    # -- small version of fillTTree
+    beamintensity = 2.29e14 ## https://acc-stats.web.cern.ch/acc-stats/#lhc/fill-details 4536, ring 1.
+    Trev  = 2*math.pi/112450
+    kT = 1.38e-23*300
+
+    # rootfile with results
+    rfoutname = resultFileBG(bbgFile,'')
+    
+    print 'writing ','.'*33, rfoutname
+    rfOUTile = TFile.Open(rfoutname, "RECREATE")
+
+    rHists,hists_flat, hists_reweighted, cnt = [],[],[], 0
+    sk = []
+    for skey in sDict.keys():
+
+        if skey.count("Sel"): continue
+        elif skey.count("Neg"): continue
+        elif skey.count("Pos"): continue
+        elif skey.count("Z"): continue
+        elif skey.count("Neu_"): continue
+        elif skey.count("Char"): continue
+        elif skey.count("Plus") or skey.count("Minus"): continue
+        elif skey.split(tag)[0].endswith("0") or skey.count("XY"): continue
+        elif skey.count("Pio") or skey.count("Kao"): continue
+
+        # for testing!!
+        # if not skey.startswith("Rad"): continue
+
+        sk += [skey]
+        
+        print "histogram ", len(sk)+1, "."*33, skey
+
+    for j,skey in enumerate(sk):
+        print "Now on #",j,">"*5, skey
+        cnt += 1
+
+        # -- x axis, value
+        particleTypes = sDict[skey][0]
+        hname         = skey
+        xnbins        = sDict[skey][2]
+        xmin          = sDict[skey][3]
+        xmax          = sDict[skey][4]
+        mt            = tBBG
+
+        var = ''
+        energyweight = ''
+        cuts = [' energy_ke > 0.02 ']
+        if skey.startswith("Ekin"):
+            xaxis = getXLogAxis(xnbins, xmin, xmax)
+            var = "energy_ke"        
+
+        elif hname.startswith("Rad"):
+            binwidth = xmax/xnbins
+            xaxis = [i*binwidth for i in range(xnbins+1)]
+            var = '(TMath::Sqrt(x*x + y*y))'
+            if skey.count("En"): energyweight = "energy_ke * "
+
+        elif hname.startswith("Phi"):
+            binwidth = (xmax-xmin)/xnbins
+            xaxis = [xmin+i*binwidth for i in range(xnbins+1)]
+            var = '(TMath::ATan2(y,x))'
+            if skey.count("En"): energyweight = "energy_ke * "
+
+        if not particleTypes[0].count('ll'):
+            pcuts = [ 'particle ==' + p for p in particleTypes  ]
+            pcut  = '||'.join(pcuts)
+            cuts += ['('+ pcut + ')']
+
+        # -- y axis, weigths
+        ynbins, ymin, ymax =  523, 22.5, 550
+        twoDhist = TH2F(skey, skey, xnbins, array('d', xaxis), ynbins, ymin, ymax)
+
+        hname_flat = skey + '_flat'
+        twoDhist_flat = twoDhist.Clone(hname_flat)
+        twoDhist_flat.Sumw2()
+
+        hname_reweighted = skey + '_reweighted'
+        twoDhist_reweighted = twoDhist.Clone(hname_reweighted)
+
+        var = '0.01*z_interact:' + var
+        cuts = "weight * "+energyweight+"("+" && ".join(cuts) + ") "
+        print "INFO: applying", cuts, "to", var, "in", hname_flat
+        mt.Project(hname_flat, var, cuts)
+
+        print "entries  ", twoDhist_flat.GetEntries()
+        hist_flat = twoDhist_flat.ProjectionX("makeit1d_flat" + skey)
+        hist_flat.SetLineColor(kTeal-2)
+
+        # -- create histogram with same axis for pint 
+        if cnt == 1:
+            hist_pint = twoDhist.ProjectionY("pint")
+            data = cv79.getdata5c(pFile)
+            rho_H2, rho_CH4, rho_CO, rho_CO2 = cv79.getrho(data['H2_Eq']),cv79.getrho(data['CH4_Eq']),cv79.getrho(data['CO_Eq']),cv79.getrho(data['CO2_Eq'])
+            rho_H, rho_C, rho_O = cv79.getAtomicRho(rho_H2, rho_CH4, rho_CO, rho_CO2)
+            pint_H, pint_C, pint_O, pint_tot = cv79.getpint(rho_H, rho_C, rho_O)
+            pint_incomingbeam = {}
+
+            # incoming beam is left side of B1
+            for i,spos in enumerate(data['s']): 
+                if spos < 0.: 
+                    z = -spos
+                    pint_incomingbeam[z] = pint_tot[i]
+                    zbin = hist_pint.FindBin(z)
+                    hist_pint.SetBinContent(zbin, pint_incomingbeam[z])
+
+            # first value is for arc
+            arcvalue = pint_tot[1]
+
+            startarc = 260.
+            startarcBin = hist_pint.FindBin(startarc)
+            for i in range(startarcBin, ynbins-1): hist_pint.SetBinContent(i,arcvalue)    
+        # --
+        # compute re-weights for each bin
+        for w in range(1,ynbins+1):
+            scale = beamintensity * hist_pint.GetBinContent(w)
+            for v in range(1,xnbins+1):
+                m = twoDhist_flat.GetBinContent(v,w)  
+                #print "Weight", scale * m, "m", m, "scale",scale, "x,y", v,w
+                twoDhist_reweighted.SetBinContent(v,w,scale * m)
+
+        hists_flat += [twoDhist_flat]          
+        hists_reweighted += [twoDhist_reweighted]
+        rHists += [skey]
+        
+        # writing two d hists
+        hists_flat[-1].Write()
+        hists_reweighted[-1].Write()
+
+    rfOUTile.Close()
+    print 'wrote ','.'*20, rfoutname
+    
+    # --
+
---
+++ AnalysisScripts/py/cv81.py
@@ -0,0 +1,163 @@
+#!/usr/bin/python
+#
+# reads histograms with reweights
+# Sept 16
+#
+# R Kwee, 2016
+# 
+# ---------------------------------------------------------------------------------
+import ROOT, sys, glob, os, math, helpers
+from ROOT import *
+from array import array
+# get function to read the data if 14 columns are present 
+from cv32 import getdata14c
+from helpers import makeTGraph, mylabel, wwwpath
+from fillTTree_dict import generate_sDict
+# --------------------------------------------------------------------------------
+def doEkin(hist,nbins):
+    for bin in range(1,nbins+1):
+        content = hist.GetBinContent(bin)
+        width   = hist.GetBinWidth(bin)
+        bcenter = hist.GetXaxis().GetBinCenterLog(bin)
+        hist.SetBinContent(bin,bcenter*content/width)
+    return hist
+
+def doPhi(hist,nbins):
+    for i in range(1,nbins+1):
+        content = hist.GetBinContent(i)
+        binWidth = hist.GetBinWidth(i)
+        hist.SetBinContent(i,content/binWidth)
+        hist.SetBinError(i,hist.GetBinError(i)/binWidth)
+    return hist
+
+def doRad(hist,nbins):
+    xaxis = hist.GetXaxis()
+    for i in range(1,nbins+1):
+        binArea = math.pi * (xaxis.GetBinUpEdge(i)**2 - xaxis.GetBinUpEdge(i-1)**2)
+        content = hist.GetBinContent(i)
+        hist.SetBinContent(i,content/binArea)
+        hist.SetBinError(i,hist.GetBinError(i)/binArea)
+    return hist
+
+# --------------------------------------------------------------------------------
+def resultFileBG(k,rel):
+    n = os.path.join(os.path.dirname(k),"results_pressure2015_"+rel+k.split('/')[-1])
+    return  n
+# --------------------------------------------------------------------------------
+def cv81():
+
+    datafile = '/afs/cern.ch/project/lhc_mib/valBG4TeV/ir1_BG_bs_4TeV_20MeV_b1_nprim5925000_67'
+    tag = '_BG_4TeV_20MeV_bs'
+    beamintensity = 2e14
+
+    energy = "6.5 TeV"
+    datafile = '/Users/rkwee/Documents/RHUL/work/HL-LHC/runs/TCT/ir1_BG_bs_6500GeV_b1_20MeV_nprim3198000_67'
+    tag = '_BG_6500GeV_flat_20MeV_bs' #!! MMMeV NOT GeV
+    beamintensity = 2.29e14 ## https://acc-stats.web.cern.ch/acc-stats/#lhc/fill-details 4536, ring 1.
+    
+    bbgFile = datafile + ".root"
+    print "Opening", bbgFile
+    norm = float(bbgFile.split('nprim')[-1].split('_')[0])
+    rfile = TFile.Open(bbgFile, "READ")
+    tBBG = rfile.Get("particle")
+    yrel = ''
+    sDict = generate_sDict(tag, norm, tBBG, yrel)
+
+    # -- small version of plotSpectra    
+    Trev  = 2*math.pi/112450
+    kT = 1.38e-23*300
+
+    # rootfile with results
+    rfoutname = resultFileBG(bbgFile,'')
+    print "Opening", "."*30,bbgFile
+    rf = TFile.Open(rfoutname, "READ")
+
+    for i,skey in enumerate(sDict.keys()):
+
+        if skey.count("Sel"): continue
+        elif skey.count("Neg"): continue
+        elif skey.count("Pos"): continue
+        elif skey.count("Z"): continue
+        elif skey.count("Neu_"): continue
+        elif skey.count("Char"): continue
+        elif skey.count("Plus") or skey.count("Minus"): continue
+        elif skey.split(tag)[0].endswith("0") or skey.count("XY"): continue
+        elif skey.count("Pio") or skey.count("Kao"): continue
+
+        cv = TCanvas(skey+ 'cv',skey+ 'cv', 1400, 900)
+
+        x1, y1, x2, y2 = 0.7, 0.75, 0.9, 0.88
+        mlegend = TLegend( x1, y1, x2, y2)
+        mlegend.SetFillColor(0)
+        mlegend.SetFillStyle(0)
+        mlegend.SetLineColor(0)
+        mlegend.SetTextSize(0.035)
+        mlegend.SetShadowColor(0)
+        mlegend.SetBorderSize(0)
+
+        twoDhname_flat = skey + '_flat'
+        twoDhname_reweighted = skey + '_reweighted'
+        twoDhist_flat = rf.Get(twoDhname_flat)
+        twoDhist_reweighted = rf.Get(twoDhname_reweighted)
+
+        print twoDhist_flat, skey
+        hist_flat = twoDhist_flat.ProjectionX(skey + "makeit1d_flat")
+        hist_reweighted = twoDhist_reweighted.ProjectionX(skey + "makeit1d_reweighted")
+        nbins = hist_reweighted.GetNbinsX()
+
+        XurMin,XurMax = -1,-1.
+        doLogx, doLogy = 0, 0
+        if skey.count("Ekin"):
+            hist_flat = doEkin(hist_flat,nbins)
+            hist_reweighted = doEkin(hist_reweighted,nbins)
+            doLogx, doLogy = 1,1
+        elif skey.count("Phi"):
+            hist_flat = doPhi(hist_flat,nbins)
+            hist_reweighted = doPhi(hist_reweighted,nbins)
+            doLogx, doLogy = 0,1
+        elif skey.count("Rad"):
+            hist_flat = doRad(hist_flat,nbins)
+            hist_reweighted = doRad(hist_reweighted,nbins)
+            hist_flat = helpers.doRebin(hist_flat,3)
+            hist_reweighted = helpers.doRebin(hist_reweighted,3)
+            doLogx, doLogy = 0,1
+            XurMin,XurMax = 0, 600.
+            
+        hname = skey
+        xtitle = sDict[hname][9]
+        ytitle = sDict[hname][10] + ' a.u.'
+
+        hist_flat.Scale(1./hist_flat.Integral())
+        hist_flat.GetXaxis().SetTitle(xtitle)
+        hist_flat.GetYaxis().SetTitle(ytitle)
+
+        if XurMin != -1:
+            hist_flat.GetXaxis().SetRangeUser(XurMin, XurMax)
+
+        hist_flat.Draw("h")
+        lg, lm = "flat", 'l'
+        mlegend.AddEntry(hist_flat, lg, lm)
+
+        hist_reweighted.SetLineColor(kAzure-3)
+        hist_reweighted.SetMarkerColor(kAzure-3)
+        hist_reweighted.Scale(1./hist_reweighted.Integral())
+        hist_reweighted.GetYaxis().SetTitle(ytitle)
+        hist_reweighted.Draw("hsame")
+        lg, lm = "reweighted", 'l'
+        mlegend.AddEntry(hist_reweighted, lg, lm)
+
+        cv.SetLogx(doLogx)
+        cv.SetLogy(doLogy)
+        gPad.RedrawAxis()
+
+        lab = mylabel(42)
+        lab.DrawLatex(0.2, 0.9, energy+' beam-gas' )
+        lab.DrawLatex(0.5, 0.82, sDict[hname][6] )
+
+        mlegend.Draw()
+
+        pname = wwwpath + 'TCT/6.5TeV/beamgas/fluka/bs/reweighted/'+skey+'.pdf'
+        pname = '/Users/rkwee/Documents/RHUL/work/HL-LHC/LHC-Collimation/Documentation/ATS/HLHaloBackgroundNote/figures/6500GeV/reweighted/' + skey + '.pdf'
+        print('Saving file as ' + pname) 
+        cv.Print(pname)
+
